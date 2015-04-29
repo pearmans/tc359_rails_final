7c8a0a84dbed136d0b55553243dbfea757434cf5
Chris Fritz <chrisvfritz@gmail.com>
2015-04-29 11:43:47 -0400

    added the beginnings of parsing post content

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,9 @@ module PostsHelper
     link_to("Card", imgur_path, class: "embedly-card") +
     javascript_include_tag("//cdn.embedly.com/widgets/platform.js")
   end
+
+  def parse_post_content(html)
+    # Add nokogiri gem to Gemfile, then something like this...
+    # Nokogiri::HTML(html).css('body').children.map(&:text).join(" ")
+  end
 end
